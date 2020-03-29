9849472ca2d5f25192ea675a821831b3cf8610f5
trenea <andrei.trenea@code4nord.com>
2020-03-29 17:57:41 +0300

    working version

---
+++ BarieraApp/obj/Debug/90/android/src/md57e055b9e91679dbda5afa73fbae8dc64/BarieraForgroundService.java
@@ -0,0 +1,74 @@
+package md57e055b9e91679dbda5afa73fbae8dc64;
+
+
+public class BarieraForgroundService
+	extends android.app.Service
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onBind:(Landroid/content/Intent;)Landroid/os/IBinder;:GetOnBind_Landroid_content_Intent_Handler\n" +
+			"n_onCreate:()V:GetOnCreateHandler\n" +
+			"n_onDestroy:()V:GetOnDestroyHandler\n" +
+			"n_onStartCommand:(Landroid/content/Intent;II)I:GetOnStartCommand_Landroid_content_Intent_IIHandler\n" +
+			"";
+		mono.android.Runtime.register ("BarieraApp.BarieraForgroundService, BarieraApp", BarieraForgroundService.class, __md_methods);
+	}
+
+
+	public BarieraForgroundService ()
+	{
+		super ();
+		if (getClass () == BarieraForgroundService.class)
+			mono.android.TypeManager.Activate ("BarieraApp.BarieraForgroundService, BarieraApp", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public android.os.IBinder onBind (android.content.Intent p0)
+	{
+		return n_onBind (p0);
+	}
+
+	private native android.os.IBinder n_onBind (android.content.Intent p0);
+
+
+	public void onCreate ()
+	{
+		n_onCreate ();
+	}
+
+	private native void n_onCreate ();
+
+
+	public void onDestroy ()
+	{
+		n_onDestroy ();
+	}
+
+	private native void n_onDestroy ();
+
+
+	public int onStartCommand (android.content.Intent p0, int p1, int p2)
+	{
+		return n_onStartCommand (p0, p1, p2);
+	}
+
+	private native int n_onStartCommand (android.content.Intent p0, int p1, int p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
